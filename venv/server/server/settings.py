62c4a17fe76c9e1fe3fe3d06ba1d13f7eb6554b3
Xavier <munroexavier@gmail.com>
2019-04-05 12:21:28 -0400

    added middleware to disable CSRF check.

--- venv/server/server/settings.py
+++ venv/server/server/settings.py
@@ -58,6 +58,7 @@ MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     # 'django.middleware.csrf.CsrfViewMiddleware',
+    'cookbook.middleware.DisableCSRFMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
