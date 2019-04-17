68f5de5ad7c80d5b0bba8411dd9930e7dd33b082
Xavier <munroexavier@gmail.com>
2019-04-17 12:56:02 -0400

    More changes to the recipe detail. Mainly styling and adding in the edit link if you are logged in.

--- client/src/components/profile/userRecipes.tsx
+++ client/src/components/profile/userRecipes.tsx
@@ -1,25 +1,29 @@
 import * as React from 'react';
-import { IUser } from '../../store/reducers/user';
-import { IRecipe } from '../../store/reducers/recipe';
-import RecipeList from '../recipeList';
 import { RouteComponentProps } from 'react-router';
+import { IRecipe } from '../../store/reducers/recipe';
+import { IUser } from '../../store/reducers/user';
+import RecipeList from '../recipeList';
 
 export interface IUserRecipesProps extends RouteComponentProps {
 	currentUser: IUser | null;
 	profileOwner: IUser | null;
-	recipes: Array<IRecipe>;
+	recipes: IRecipe[];
 }
 
 const RecipeHeader = (currentUser: IUser | null, profileOwner: IUser | null): React.ReactNode => {
-    if (!profileOwner) return <h1>NOT FOUND</h1>;
-    if (currentUser && currentUser.pk === profileOwner.id) return <h1>Your Recipes</h1>;
-    return <h1>{profileOwner.username}'s Recipes</h1>
+	if (!profileOwner) {
+		return <h1>NOT FOUND</h1>;
+	}
+	if (currentUser && currentUser.pk === profileOwner.id) {
+		return <h1>Your Recipes</h1>;
+	}
+	return <h1>{profileOwner.username}'s Recipes</h1>;
 };
 
 const UserRecipes: React.FC<IUserRecipesProps> = (props: IUserRecipesProps) => {
 	return (
-        <div className='recipe-list-cont'>
-            {RecipeHeader(props.currentUser, props.profileOwner)}
+		<div className='recipe-list-cont'>
+			{RecipeHeader(props.currentUser, props.profileOwner)}
 			<RecipeList recipes={props.recipes} currentUser={props.currentUser} profileOwner={props.profileOwner} />
 		</div>
 	);
--- client/src/components/recipeDetail.tsx
+++ client/src/components/recipeDetail.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { Link } from 'react-router-dom';
 import { IRecipe } from '../store/reducers/recipe';
 import { IUser } from '../store/reducers/user';
 
@@ -9,20 +10,47 @@ interface IRecipeDetailProps extends IRecipe {
 }
 
 const RecipeDetail: React.FC<IRecipeDetailProps> = (props: IRecipeDetailProps) => {
+	const handleEditLink = (): JSX.Element | null => {
+		if (props.currentUser && props.profileOwner) {
+			if (props.currentUser.pk === props.profileOwner.id) {
+				return( <span>
+				<Link to='/edit'>Edit Recipe</Link>
+			</span>);
+			}
+			return null;
+		}
+		return null;
+	};
 	return (
-		<div>
+		<div className='recipe-detail-cont'>
 			<section className='recipe-detail-header-section'>
-				<h2>{props.recipe_name}</h2>
-				<span onClick={props.closeRecipeDetail}>X</span>
+				<div className='r-detail-name'>
+					<h2>{props.recipe_name}</h2>
+					<span onClick={props.closeRecipeDetail}>X</span>
+				</div>
+				<div className='r-detail-owner'>
+					<span>
+						By:{' '}
+						{
+							props.profileOwner ? props.profileOwner.username :
+							''}
+					</span>
+				</div>
 			</section>
 			<section className='recipe-detail-image-section'>
 				<img src={props.image || ''} alt='recipe image' />
 			</section>
 			<section className='recipe-detail-content'>
-				<div>
-					<span>Favorites: {props.favorite_count}</span>
+				<div className='recipe-stats-links'>
+					<div>{handleEditLink()}</div>
+					<div>
+						<span>Favorites: {props.favorite_count}</span>
+					</div>
 				</div>
-				<div>{props.description}</div>
+				<div>
+					<h4>Description</h4>
+					<p>{props.description}</p>
+				</div>
 				<div>
 					<h4>Ingredients</h4>
 					{props.ingredients.map((ingred, i) => <p key={i}>{ingred}</p>)}
